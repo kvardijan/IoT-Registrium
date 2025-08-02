fa48d5df3c75c3b89a425b5540e73f16d2b46c39
Karlo Vardijan <kvardijan20@foi.hr>
2025-08-02 18:13:14 +0200

    Implemented device registration and get by id. Created generic Api response class for server responses.

--- device-service/Controllers/DeviceController.cs
+++ device-service/Controllers/DeviceController.cs
@@ -1,16 +1,45 @@
-﻿using device_service.Models;
+﻿using device_service.DTOs;
+using device_service.Models;
+using device_service.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace device_service.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class DeviceController : Controller
+    public class DeviceController : ControllerBase
     {
-        private readonly DevicesDbContext _context;
-        public DeviceController(DevicesDbContext context)
+        private readonly DeviceService _deviceService;
+        public DeviceController(DeviceService deviceService)
         {
-            _context = context;
+            _deviceService = deviceService;
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult RegisterDevice([FromBody] DeviceRegistrationDto deviceRegistrationDto)
+        {
+            var device = _deviceService.RegisterDevice(deviceRegistrationDto);
+
+            if (device == null)
+            {
+                return BadRequest(ApiResponse<object>.Fail("Failed to register device.", 400));
+            }
+
+            return CreatedAtAction(nameof(GetDeviceById), new { id = device.Id }, ApiResponse<Device>.Ok(device));
+        }
+
+        [Authorize]
+        [HttpGet("{id}")]
+        public IActionResult GetDeviceById(int id)
+        {
+            var device = _deviceService.GetDeviceById(id);
+            if (device == null)
+            {
+                return NotFound(ApiResponse<object>.Fail("Device not found", 404));
+            }
+            return Ok(ApiResponse<Device>.Ok(device));
         }
     }
 }
--- device-service/Services/DeviceService.cs
+++ device-service/Services/DeviceService.cs
@@ -1,6 +1,43 @@
-﻿namespace device_service.Services
+﻿using device_service.DTOs;
+using device_service.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace device_service.Services
 {
     public class DeviceService
     {
+        private readonly DevicesDbContext _context;
+        public DeviceService(DevicesDbContext context)
+        {
+            _context = context;
+        }
+
+        public Device? RegisterDevice(DeviceRegistrationDto deviceRegistrationDto)
+        {
+            Device newDevice = MapDevice(deviceRegistrationDto);
+            _context.Devices.Add(newDevice);
+            _context.SaveChanges();
+            return newDevice;
+        }
+
+        public Device? GetDeviceById(int id)
+        {
+            return _context.Devices.FirstOrDefault(d => d.Id == id);
+        }
+
+        private Device MapDevice(DeviceRegistrationDto deviceRegistrationDto)
+        {
+            return new Device
+            {
+                SerialNumber = deviceRegistrationDto.SerialNumber,
+                Model = deviceRegistrationDto.Model,
+                Manufacturer = deviceRegistrationDto.Manufacturer,
+                Type = deviceRegistrationDto.Type,
+                Status = deviceRegistrationDto.Status,
+                FirmwareVersion = deviceRegistrationDto.FirmwareVersion,
+                Location = deviceRegistrationDto.Location,
+                LastSeen = DateTime.Now
+            };
+        }
     }
 }
