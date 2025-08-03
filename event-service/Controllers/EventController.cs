da6b9828820aa89360187e5e38e4525dd836dc5f
Karlo Vardijan <kvardijan20@foi.hr>
2025-08-03 20:20:51 +0200

    Implemented endpoints for getting all events and all events for a specified device serial number.

--- event-service/Controllers/EventController.cs
+++ event-service/Controllers/EventController.cs
@@ -1,17 +1,42 @@
 ﻿using event_service.Models;
 using event_service.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace event_service.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class EventController : Controller
+    public class EventController : ControllerBase
     {
         private readonly EventService _eventService;
         public EventController(EventService eventService)
         {
             _eventService = eventService;
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult GetEvents()
+        {
+            var events = _eventService.GetEvents();
+            if (events == null)
+            {
+                return NotFound(ApiResponse<object>.Fail("No events found.", 404));
+            }
+            return Ok(ApiResponse<List<Event>>.Ok(events));
+        }
+
+        [Authorize]
+        [HttpGet("{serialNumber}")]
+        public IActionResult GetEventsOfDevice(string serialNumber)
+        {
+            var events = _eventService.GetEventsOfDevice(serialNumber);
+            if (events == null)
+            {
+                return NotFound(ApiResponse<object>.Fail("No events found for device.", 404));
+            }
+            return Ok(ApiResponse<List<Event>>.Ok(events));
         }
     }
 }
--- event-service/Services/EventService.cs
+++ event-service/Services/EventService.cs
@@ -9,5 +9,15 @@ namespace event_service.Services
         {
             _context = context;
         }
+
+        public List<Event> GetEvents()
+        {
+            return _context.Events.ToList();
+        }
+
+        public List<Event>? GetEventsOfDevice(string serialNumber)
+        {
+            return _context.Events.Where(e => e.Device == serialNumber).ToList();
+        }
     }
 }
