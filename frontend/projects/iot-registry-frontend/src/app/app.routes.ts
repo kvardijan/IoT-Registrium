170f958c606bd93a6eef04f288aba25865b7d375
Karlo Vardijan <kvardijan20@foi.hr>
2025-08-04 22:52:17 +0200

    Added blank component for devices table along with routes.

--- frontend/projects/iot-registry-frontend/src/app/app.routes.ts
+++ frontend/projects/iot-registry-frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { DevicesTable } from './devices-table/devices-table';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: DevicesTable},
+    {path: 'devicesTable', component: DevicesTable}
+];
---
+++ frontend/projects/iot-registry-frontend/src/app/devices-table/devices-table.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-devices-table',
+  imports: [],
+  templateUrl: './devices-table.html',
+  styleUrl: './devices-table.scss'
+})
+export class DevicesTable {
+
+}
