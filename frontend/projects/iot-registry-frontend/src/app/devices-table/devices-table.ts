fb7e7e8481f96f030a229eb30d6479a5988f2120
Karlo Vardijan <kvardijan20@foi.hr>
2025-08-05 15:31:36 +0200

    Implemented table view of all devices on landing page (without location desc).

--- frontend/projects/iot-registry-frontend/src/app/devices-table/devices-table.ts
+++ frontend/projects/iot-registry-frontend/src/app/devices-table/devices-table.ts
@@ -1,11 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-devices-table',
-  imports: [],
+    standalone: true, 
+  imports: [CommonModule],
   templateUrl: './devices-table.html',
   styleUrl: './devices-table.scss'
 })
-export class DevicesTable {
+export class DevicesTable implements OnInit {
+  devices: any[] = [];
 
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+    this.fetchDevices();
+  }
+
+  fetchDevices() {
+    this.http.get<any>('http://localhost:5208/api/device')
+      .subscribe({
+        next: (response) => {
+          if (response.success) {
+            this.devices = response.data;
+            console.log(this.devices);
+          } else {
+            console.error('Failed to fetch devices:', response.error);
+          }
+        },
+        error: (err) => {
+          console.error('Error fetching devices', err);
+        }
+      });
+  }
+
+  getStatusClass(status: string): string {
+  switch (status.toLowerCase()) {
+    case 'active':
+      return 'green';
+    case 'idle':
+      return 'yellow';
+    case 'deactivated':
+      return 'gray';
+    case 'error':
+      return 'red';
+    default:
+      return '';
+  }
+}
 }
