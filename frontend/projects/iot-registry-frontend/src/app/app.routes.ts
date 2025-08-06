0e0c5434fa72bd82848d1bbed066b52cd6375453
Karlo Vardijan <kvardijan20@foi.hr>
2025-08-06 22:32:55 +0200

    Created blank add device component.

--- frontend/projects/iot-registry-frontend/src/app/app.routes.ts
+++ frontend/projects/iot-registry-frontend/src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { DevicesTable } from './devices-table/devices-table';
 import { NotFound } from './not-found/not-found';
 import { Login } from './login/login';
 import { LocationsTable } from './locations-table/locations-table';
+import { AddDevice } from './add-device/add-device';
 import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
     {path: 'devicestable', component: DevicesTable},
     {path: 'login', component: Login},
-    { path: 'locations', component: LocationsTable, canActivate: [authGuard] },
+    {path: 'locations', component: LocationsTable, canActivate: [authGuard]},
+    {path: 'adddevice', component: AddDevice, canActivate: [authGuard]},
     {path: '', component: DevicesTable},
     {path: '**', component: NotFound}
 ];
