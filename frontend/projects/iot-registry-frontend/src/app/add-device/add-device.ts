2931301da27ff38021fd455ed2099a0ce4612384
Karlo Vardijan <kvardijan20@foi.hr>
2025-08-06 23:09:21 +0200

    Created form for registering new devices.

--- frontend/projects/iot-registry-frontend/src/app/add-device/add-device.ts
+++ frontend/projects/iot-registry-frontend/src/app/add-device/add-device.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-device',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './add-device.html',
   styleUrl: './add-device.scss'
 })
 export class AddDevice {
+  message = '';
+  serialNumber = '';
+  model = '';
+  manufacturer = '';
+  firmware = '';
+  type = -1;
+  status = -1;
+  location = -1;
 
+  types: any[] = [];
+  statuses: any[] = [];
+  locations: any[] = [];
 }
