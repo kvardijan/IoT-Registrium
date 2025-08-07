e1a5ec83ac0317ff6a05e0ca15fc90c41bc2cf62
Karlo Vardijan <kvardijan20@foi.hr>
2025-08-07 21:14:30 +0200

    Implemented fetching of events for device.

--- frontend/projects/iot-registry-frontend/src/app/device-events/device-events.ts
+++ frontend/projects/iot-registry-frontend/src/app/device-events/device-events.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
+import { HttpClient } from '@angular/common/http';
+import { UserManagerService } from '../user-manager-service';
 @Component({
   selector: 'app-device-events',
   standalone: true,
@@ -10,11 +11,35 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class DeviceEvents implements OnInit {
   serialNumber: string = '';
+  events: any[] = [];
 
-  constructor(private route: ActivatedRoute){}
+  constructor(private route: ActivatedRoute, private http: HttpClient, public userManager: UserManagerService) { }
 
   ngOnInit(): void {
     this.serialNumber = this.route.snapshot.paramMap.get('serialNumber') || '';
+    this.fetchEvents();
     console.log('Serial number from route:', this.serialNumber);
+  }
+
+  fetchEvents() {
+    const jwt = this.userManager.getToken();
+    const headers = {
+      Authorization: 'Bearer ' + jwt
+    };
+
+    this.http.get<any>('http://localhost:5282/api/event/device/' + this.serialNumber, { headers })
+      .subscribe({
+        next: (response) => {
+          if (response.success) {
+            this.events = response.data;
+            console.log(this.events);
+          } else {
+            console.error('Failed to fetch events:', response.error);
+          }
+        },
+        error: (err) => {
+          console.error('Error fetching events', err);
+        }
+      });
   }
 }
