42597a333d525d56960560ee1ff74c0dae87952b
Karlo Vardijan <kvardijan20@foi.hr>
2025-08-07 20:51:49 +0200

    Created blank component for view of events for a specific device.

---
+++ frontend/projects/iot-registry-frontend/src/app/device-events/device-events.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-device-events',
+  standalone: true,
+  imports: [],
+  templateUrl: './device-events.html',
+  styleUrl: './device-events.scss'
+})
+export class DeviceEvents {
+
+}
