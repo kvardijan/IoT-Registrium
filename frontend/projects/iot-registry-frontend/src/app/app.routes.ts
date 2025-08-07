aed41efc7acdf6f6dc222d0b7b5c14caf624ca41
Karlo Vardijan <kvardijan20@foi.hr>
2025-08-07 21:05:41 +0200

    Implemented routing to device events component on serial number click.

--- frontend/projects/iot-registry-frontend/src/app/app.routes.ts
+++ frontend/projects/iot-registry-frontend/src/app/app.routes.ts
@@ -5,12 +5,14 @@ import { Login } from './login/login';
 import { LocationsTable } from './locations-table/locations-table';
 import { AddDevice } from './add-device/add-device';
 import { authGuard } from './auth.guard';
+import { DeviceEvents } from './device-events/device-events';
 
 export const routes: Routes = [
     {path: 'devicestable', component: DevicesTable},
     {path: 'login', component: Login},
     {path: 'locations', component: LocationsTable, canActivate: [authGuard]},
     {path: 'adddevice', component: AddDevice, canActivate: [authGuard]},
+    {path: 'devices/:serialNumber/events', component: DeviceEvents, canActivate: [authGuard]},
     {path: '', component: DevicesTable},
     {path: '**', component: NotFound}
 ];
--- frontend/projects/iot-registry-frontend/src/app/device-events/device-events.ts
+++ frontend/projects/iot-registry-frontend/src/app/device-events/device-events.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-device-events',
@@ -7,6 +8,13 @@ import { Component } from '@angular/core';
   templateUrl: './device-events.html',
   styleUrl: './device-events.scss'
 })
-export class DeviceEvents {
+export class DeviceEvents implements OnInit {
+  serialNumber: string = '';
 
+  constructor(private route: ActivatedRoute){}
+
+  ngOnInit(): void {
+    this.serialNumber = this.route.snapshot.paramMap.get('serialNumber') || '';
+    console.log('Serial number from route:', this.serialNumber);
+  }
 }
