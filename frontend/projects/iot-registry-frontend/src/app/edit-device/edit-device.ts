35f811818f896a2c664d9ba301f04d19302c345e
Karlo Vardijan <kvardijan20@foi.hr>
2025-08-10 00:38:18 +0200

    Filled form with existing device data.

--- frontend/projects/iot-registry-frontend/src/app/edit-device/edit-device.ts
+++ frontend/projects/iot-registry-frontend/src/app/edit-device/edit-device.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { ActivatedRoute } from '@angular/router';
+import { UserManagerService } from '../user-manager-service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-edit-device',
@@ -10,7 +13,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
   templateUrl: './edit-device.html',
   styleUrl: './edit-device.scss'
 })
-export class EditDevice {
+export class EditDevice implements OnInit {
+  serialNumber = '';
   message = '';
   model = '';
   manufacturer = '';
@@ -23,7 +27,101 @@ export class EditDevice {
   statuses: any[] = [];
   locations: any[] = [];
 
-  editDevice(){
-    
+  constructor(private route: ActivatedRoute, private http: HttpClient, public userManager: UserManagerService) { }
+
+  ngOnInit(): void {
+    this.serialNumber = this.route.snapshot.paramMap.get('serialNumber') || '';
+    console.log(this.serialNumber);
+    this.fetchDevice();
+    this.fetchTypes();
+    this.fetchStatuses();
+    this.fetchLocations();
+  }
+
+  fetchDevice() {
+    this.http.get<any>('http://localhost:5208/api/device/serial/' + this.serialNumber)
+      .subscribe({
+        next: (response) => {
+          if (response.success) {
+            console.log(response.data);
+            this.model = response.data.model;
+            this.manufacturer = response.data.manufacturer;
+            this.firmware = response.data.firmwareVersion;
+            this.type = response.data.typeId;
+            this.status = response.data.statusId;
+            this.location = response.data.location;
+          } else {
+            console.error('Failed to fetch device:', response.error);
+          }
+        },
+        error: (err) => {
+          console.error('Error fetching device', err);
+        }
+      });
+  }
+
+  fetchStatuses() {
+    const jwt = this.userManager.getToken();
+    const headers = {
+      Authorization: 'Bearer ' + jwt
+    };
+
+    this.http.get<any>('http://localhost:5208/api/device/statuses', { headers })
+      .subscribe({
+        next: (response) => {
+          if (response.success) {
+            this.statuses = response.data;
+            console.log(this.statuses);
+          } else {
+            console.error('Failed to fetch statuses:', response.error);
+          }
+        },
+        error: (err) => {
+          console.error('Error fetching statuses', err);
+        }
+      });
+  }
+
+  fetchTypes() {
+    const jwt = this.userManager.getToken();
+    const headers = {
+      Authorization: 'Bearer ' + jwt
+    };
+
+    this.http.get<any>('http://localhost:5208/api/device/types', { headers })
+      .subscribe({
+        next: (response) => {
+          if (response.success) {
+            this.types = response.data;
+            console.log(this.types);
+          } else {
+            console.error('Failed to fetch types:', response.error);
+          }
+        },
+        error: (err) => {
+          console.error('Error fetching types', err);
+        }
+      });
+  }
+
+  fetchLocations() {
+    this.http.get<any>('http://localhost:5261/api/location')
+      .subscribe({
+        next: (response) => {
+          if (response.success) {
+            this.locations = response.data;
+          } else {
+            console.error('Failed to fetch locations:', response.error);
+          }
+        },
+        error: (err) => {
+          console.error('Error fetching locations', err);
+        }
+      });
+  }
+
+
+  editDevice() {
+
   }
 }
