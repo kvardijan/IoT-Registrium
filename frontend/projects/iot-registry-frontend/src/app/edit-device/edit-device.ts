f63c6bc33dc0c5572be9fd0ea966d43a8f5112dd
Karlo Vardijan <kvardijan20@foi.hr>
2025-08-09 23:43:18 +0200

    Added form elements and css to the editing.

--- frontend/projects/iot-registry-frontend/src/app/edit-device/edit-device.ts
+++ frontend/projects/iot-registry-frontend/src/app/edit-device/edit-device.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-edit-device',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, MatSelectModule, MatFormFieldModule],
   templateUrl: './edit-device.html',
   styleUrl: './edit-device.scss'
 })
 export class EditDevice {
+  message = '';
+  model = '';
+  manufacturer = '';
+  firmware = '';
+  type = -1;
+  status = -1;
+  location = -1;
 
+  types: any[] = [];
+  statuses: any[] = [];
+  locations: any[] = [];
+
+  editDevice(){
+    
+  }
 }
