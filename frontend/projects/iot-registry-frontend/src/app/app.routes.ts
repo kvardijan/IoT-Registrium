053ea72995715b54a58d0997aa15373e02a72421
Karlo Vardijan <kvardijan20@foi.hr>
2025-08-11 20:23:37 +0200

    Added map to the devicesMap component with default coordinates set to Varazdin.

--- frontend/projects/iot-registry-frontend/src/app/app.routes.ts
+++ frontend/projects/iot-registry-frontend/src/app/app.routes.ts
@@ -6,13 +6,15 @@ import { LocationsTable } from './locations-table/locations-table';
 import { AddDevice } from './add-device/add-device';
 import { authGuard } from './auth.guard';
 import { DeviceEvents } from './device-events/device-events';
+import { DevicesMap } from './devices-map/devices-map';
 
 export const routes: Routes = [
-    {path: 'devicestable', component: DevicesTable},
-    {path: 'login', component: Login},
-    {path: 'locations', component: LocationsTable, canActivate: [authGuard]},
-    {path: 'adddevice', component: AddDevice, canActivate: [authGuard]},
-    {path: 'devices/:serialNumber/events', component: DeviceEvents, canActivate: [authGuard]},
-    {path: '', component: DevicesTable},
-    {path: '**', component: NotFound}
+    { path: 'devicestable', component: DevicesTable },
+    { path: 'devicesmap', component: DevicesMap },
+    { path: 'login', component: Login },
+    { path: 'locations', component: LocationsTable, canActivate: [authGuard] },
+    { path: 'adddevice', component: AddDevice, canActivate: [authGuard] },
+    { path: 'devices/:serialNumber/events', component: DeviceEvents, canActivate: [authGuard] },
+    { path: '', component: DevicesTable },
+    { path: '**', component: NotFound }
 ];
--- frontend/projects/iot-registry-frontend/src/app/devices-map/devices-map.ts
+++ frontend/projects/iot-registry-frontend/src/app/devices-map/devices-map.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import { fromLonLat } from 'ol/proj';
 
 @Component({
   selector: 'app-devices-map',
+  standalone: true,
   imports: [],
   templateUrl: './devices-map.html',
   styleUrl: './devices-map.scss'
 })
 export class DevicesMap {
+  varazdinLatitude = 46.3057;
+  varazdinLongitude = 16.3366;
 
+  ngAfterViewInit(): void {
+    new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new OSM()
+        })
+      ],
+      view: new View({
+        center: fromLonLat([this.varazdinLongitude, this.varazdinLatitude]),
+        zoom: 14
+      }),
+      controls: [],
+    });
+  }
 }
