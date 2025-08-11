c9b512c3c6c77677788bd5710ad74303ac60ec2d
Karlo Vardijan <kvardijan20@foi.hr>
2025-08-11 19:56:35 +0200

    Created blank component for device map view.

---
+++ frontend/projects/iot-registry-frontend/src/app/devices-map/devices-map.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-devices-map',
+  imports: [],
+  templateUrl: './devices-map.html',
+  styleUrl: './devices-map.scss'
+})
+export class DevicesMap {
+
+}
