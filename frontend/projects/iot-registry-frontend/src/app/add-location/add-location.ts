83df09b92eb0b172e56c345d118f245426de193c
Karlo Vardijan <kvardijan20@foi.hr>
2025-08-12 21:27:32 +0200

    Created basic form for location adding.

--- frontend/projects/iot-registry-frontend/src/app/add-location/add-location.ts
+++ frontend/projects/iot-registry-frontend/src/app/add-location/add-location.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { UserManagerService } from '../user-manager-service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-location',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './add-location.html',
   styleUrl: './add-location.scss'
 })
-export class AddLocation {
+export class AddLocation implements OnInit {
+  message = '';
+  address = '';
+  description = '';
+  longitude = 0;
+  latitude = 0;
 
+  constructor(private http: HttpClient, public userManager: UserManagerService) { }
+
+  ngOnInit(): void {
+
+  }
+
+  addLocation() {
+
+  }
 }
