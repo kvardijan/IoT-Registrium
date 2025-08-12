709fc7e88dd209d5cae629681c6596e4c94c50ea
Karlo Vardijan <kvardijan20@foi.hr>
2025-08-12 20:55:57 +0200

    Added empty component for location adding.

---
+++ frontend/projects/iot-registry-frontend/src/app/add-location/add-location.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-location',
+  imports: [],
+  templateUrl: './add-location.html',
+  styleUrl: './add-location.scss'
+})
+export class AddLocation {
+
+}
