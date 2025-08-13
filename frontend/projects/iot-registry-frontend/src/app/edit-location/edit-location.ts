28576ebd3c49f9c2ded45205a23a040b2757151f
Karlo Vardijan <kvardijan20@foi.hr>
2025-08-13 22:31:56 +0200

    Added blank component for location editing.

---
+++ frontend/projects/iot-registry-frontend/src/app/edit-location/edit-location.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-location',
+  imports: [],
+  templateUrl: './edit-location.html',
+  styleUrl: './edit-location.scss'
+})
+export class EditLocation {
+
+}
