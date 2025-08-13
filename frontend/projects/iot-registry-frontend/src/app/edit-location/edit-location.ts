5b647e9f2813024ae90c6f20d71a9f80afa5c1b2
Karlo Vardijan <kvardijan20@foi.hr>
2025-08-13 23:01:55 +0200

    Added link to location address that leads to edit page.

--- frontend/projects/iot-registry-frontend/src/app/edit-location/edit-location.ts
+++ frontend/projects/iot-registry-frontend/src/app/edit-location/edit-location.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { UserManagerService } from '../user-manager-service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import OLMap from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import { fromLonLat, toLonLat } from 'ol/proj';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+import Feature from 'ol/Feature';
+import Point from 'ol/geom/Point';
+import { Style, Fill, Stroke, Circle as CircleStyle } from 'ol/style';
 
 @Component({
   selector: 'app-edit-location',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './edit-location.html',
   styleUrl: './edit-location.scss'
 })
 export class EditLocation {
+  message = '';
+  address = '';
+  description = '';
+  longitude = 15.8724;
+  latitude = 46.1605;
 
+  constructor(private http: HttpClient, public userManager: UserManagerService) { }
+
+  editLocation(){
+
+  }
 }
