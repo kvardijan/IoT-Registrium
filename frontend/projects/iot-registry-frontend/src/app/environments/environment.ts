016ee0da54dc41a570caf43607e2a906b89ec6c0
Karlo Vardijan <kvardijan20@foi.hr>
2025-08-28 17:52:17 +0200

    Configured env for simulation endpoint and created empty component.

--- frontend/projects/iot-registry-frontend/src/app/environments/environment.ts
+++ frontend/projects/iot-registry-frontend/src/app/environments/environment.ts
@@ -3,6 +3,7 @@ export const environment = {
   deviceApi: 'http://localhost:5208/api/device',
   locationApi: 'http://localhost:5261/api/location',
   eventApi: 'http://localhost:5282/api/event',
+  simulationApi: 'http://localhost:5282/api/simulation',
   userApi: 'http://localhost:5081/api/user',
   statisticApi: 'http://localhost:5082/api/statistic'
 };
---
+++ frontend/projects/iot-registry-frontend/src/app/simulation/simulation.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-simulation',
+  standalone: true,
+  imports: [],
+  templateUrl: './simulation.html',
+  styleUrl: './simulation.scss'
+})
+export class Simulation {
+
+}
