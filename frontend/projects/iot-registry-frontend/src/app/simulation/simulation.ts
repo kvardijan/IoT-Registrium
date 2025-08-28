470776cc05a5b3c06a6ce3337c55c26667725ad5
Karlo Vardijan <kvardijan20@foi.hr>
2025-08-28 17:55:57 +0200

    Added device fetching.

--- frontend/projects/iot-registry-frontend/src/app/simulation/simulation.ts
+++ frontend/projects/iot-registry-frontend/src/app/simulation/simulation.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { UserManagerService } from '../user-manager-service';
 
 @Component({
   selector: 'app-simulation',
@@ -7,6 +11,28 @@ import { Component } from '@angular/core';
   templateUrl: './simulation.html',
   styleUrl: './simulation.scss'
 })
-export class Simulation {
+export class Simulation implements OnInit {
+  devices: any[] = [];
 
+  constructor(private http: HttpClient, public userManager: UserManagerService) { }
+
+  ngOnInit(): void {
+    this.fetchDevices();
+  }
+
+  fetchDevices() {
+    this.http.get<any>(environment.deviceApi)
+      .subscribe({
+        next: (response) => {
+          if (response.success) {
+            this.devices = response.data;
+          } else {
+            console.error('Failed to fetch devices:', response.error);
+          }
+        },
+        error: (err) => {
+          console.error('Error fetching devices', err);
+        }
+      });
+  }
 }
