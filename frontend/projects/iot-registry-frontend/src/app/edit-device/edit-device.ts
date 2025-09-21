2e8012c31c88c344ff74df46172f0c80bb26077e
Karlo Vardijan <kvardijan20@foi.hr>
2025-09-21 21:42:54 +0200

    Fix for device edit endpoint call

--- frontend/projects/iot-registry-frontend/src/app/edit-device/edit-device.ts
+++ frontend/projects/iot-registry-frontend/src/app/edit-device/edit-device.ts
@@ -147,7 +147,7 @@ export class EditDevice implements OnInit {
       body.location = this.location;
     }
 
-    this.http.patch<{ success: boolean; data: string; error: string }>(environment.deviceApi + this.id, body, { headers })
+    this.http.patch<{ success: boolean; data: string; error: string }>(environment.deviceApi + '/' + this.id, body, { headers })
       .subscribe({
         next: (response) => {
           if (response.success) {
